66f3cfa567bac8682b80714f75d0e069b9279e97
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-04-29 11:05:02 +0530

    Add files via upload

---
+++ challenge12.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        sum=sum+n%10;
+        n/=10;
+    }
+    printf("%d",sum);
+    return 0;
+}
