cd7ae52792b6fb854b2fdf55747900a2a12b9265
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-04-29 11:54:52 +0530

    Update challenge14.c

--- challenge14.c
+++ challenge14.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
     int x1,x2,y1,y2;
     printf("Enter the values : ");
     scanf("%d%d%d%d",&x1,&x2,&y1,&y2);
-    int result = 
-    printf("%d",sum);
+    int result = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+    printf("%d",result);
     return 0;
 }
