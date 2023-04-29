0eb4cbc979f6e9ad7e59e6ce3da250fbeac5e65b
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-04-29 11:05:22 +0530

    Add files via upload

---
+++ challenge14.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int x1,x2,y1,y2;
+    printf("Enter the values : ");
+    scanf("%d%d%d%d",&x1,&x2,&y1,&y2);
+    int result = 
+    printf("%d",sum);
+    return 0;
+}
