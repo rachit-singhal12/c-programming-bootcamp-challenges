32922e4b02ec1818a950ffb290ebe24c07ecd37a
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-04-30 08:05:39 +0530

    Add files via upload

---
+++ challenge16.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a;
+    printf("Enter the value : ");
+    scanf("%d",&a);
+    if(a%2==0)
+    printf("even");
+    else
+    printf("odd");
+    return 0;
+}
