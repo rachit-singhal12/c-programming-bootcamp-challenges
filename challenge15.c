18cd4b48c3130f49defa8499f4a6375711f2b01a
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-04-30 08:05:24 +0530

    Add files via upload

---
+++ challenge15.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b;
+    printf("Enter the values : ");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    printf("a is greater");
+    else
+    printf("B is greater");
+    return 0;
+}
