366cd92bb3c04fa907b3d599b460ffb29d23e498
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-05-01 20:01:59 +0530

    Add files via upload

---
+++ challenge19.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the both number : ");
+    scanf("%d%d",&a,&b);
+    if(a==b)
+    printf("True");
+    else
+    printf("False");
+    return 0;
+}
