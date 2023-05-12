a189552835a90ec3b91393c7efcc1cd9eae2e32b
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-05-12 22:48:42 +0530

    Add files via upload

---
+++ challenge35.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num,sum=0;
+    scanf("%d",&num);
+    for(int i=0;i<=num;i++)
+    {
+        if(i%3==0 || i%5==0)
+        {
+            sum+=i;
+        }
+    }
+    printf("%d",sum);
+    return 0;
+}
